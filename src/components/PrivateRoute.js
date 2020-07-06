682b610ea488c2a83e8607994a1568340c1556c0
Pablo Zarate <pablozarate_lol@hotmail.com>
2020-07-06 03:54:59 -0500

    fixed

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -3,16 +3,6 @@ import { Route, Redirect } from "react-router-dom";
 import { cmsAPI } from "../utils/http-client";
 const PrivateRoute = ({ component: Component, ...rest }) => (
     <Route {...rest} render={(props) => (
-      cmsAPI.get("/auth")
-      .then(response=>{
-        console.log("PR-Response",response)
-      })
-      .catch(err=>{
-        localStorage.removeItem("logged");
-        localStorage.removeItem("user");
-        localStorage.removeItem("userJWT");
-        props.history.push('/login');
-      }),
       localStorage.getItem("logged")
         ? <Component {...props}/>
         : <Redirect to={{
